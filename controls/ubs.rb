7d39694f6e7a516110ceb585f6396e6aaf8fa539
lhasadreams <lhasadreams@users.noreply.github.com>
2019-03-12 13:44:30 +0000

    Update

--- controls/ubs.rb
+++ controls/ubs.rb
@@ -2,7 +2,7 @@ control 'UBS-1' do
   impact 1.0
   title 'Check that the "UBS" file exits'
   desc 'A critical security file must always be installed on every machine'
-  describe file('/lbgcheck') do
+  describe file('/ubscheck') do
     it { should exist }
     its('owner') { should eq 'root' }
     its('group') { should eq 'root' }
@@ -13,6 +13,11 @@ control 'UBS-1' do
 end
 
 require_controls 'ubuntu' do
+  control 'xccdf_org.cisecurity.benchmarks_rule_2.3.4_Ensure_telnet_client_is_not_installed'
   control 'xccdf_org.cisecurity.benchmarks_rule_5.2.9_Ensure_SSH_PermitEmptyPasswords_is_disabled'
   control 'xccdf_org.cisecurity.benchmarks_rule_6.2.20_Ensure_shadow_group_is_empty'
 end
+
+# include_controls 'ubuntu' do
+#   skip_control 'xccdf_org.cisecurity.benchmarks_rule_2.3.4_Ensure_telnet_client_is_not_installed'
+# end
